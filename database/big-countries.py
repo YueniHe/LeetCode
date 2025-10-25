60e9135254e481587cf4df57704de45c5871a2ab
Yueni He <165487491+YueniHe@users.noreply.github.com>
2025-10-25 14:05:55 +0200

    LC-big-countries [Runtime: 256 ms; Memory: 69.1 MB]

--- database/big-countries.py
+++ database/big-countries.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
-    big_countries_df = [(world['area'] >= 3000000) | (world['population'] >= 25000000)]
-    return big_countries_de
+    df = world[(world['area'] >= 3000000) | (world['population'] >= 25000000)]
+    return df[['name', 'population', 'area']]
 

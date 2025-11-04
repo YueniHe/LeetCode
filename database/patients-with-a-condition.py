036e8ff722740e5f10c00b4ad86c6a9754146569
Yueni He <165487491+YueniHe@users.noreply.github.com>
2025-11-04 16:15:21 +0100

    LC-patients-with-a-condition [Runtime: 262 ms; Memory: 66.8 MB]

--- database/patients-with-a-condition.py
+++ database/patients-with-a-condition.py
@@ -1,4 +1,4 @@
 import pandas as pd
 def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
-    find_patients = patients[patients['conditions'].str.contains(r'\bDIAB1')]
+    find_patients = patients[patients['conditions'].str.contains(r'(^| )DIAB1')]
     return find_patients
